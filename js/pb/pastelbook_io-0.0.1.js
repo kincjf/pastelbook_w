e0b6ace277b7ae32af4e18200ba50ffdf606047b
root <root@dev.hitit.kr>
2014-10-22 18:04:02 +0900

    pb.io.ajax.db 추가 (_section,_params,_handler)

--- js/pb/pastelbook_io-0.0.1.js
+++ js/pb/pastelbook_io-0.0.1.js
@@ -8,7 +8,29 @@ require(['jquery', 'pastelbook_ui'], function($,pastelbook_ui){
 	$(function () {
 		pb.io.ajax = {};
 		pb.io.ajax.getProjects = function(){ // 유저의 프로젝트 가져오기	
-			console.log('aaaa');
+			//console.log('aaaa');
 		}
-	});
+		var result = null;
+		pb.io.ajax.db = function ( _section, _params, _handler ) {
+			var section = 'resource'; //  pb_resource
+			var url = 'admin/dbupdate.jsp'; // /ajax
+			var params = 'pb'; // pb 소유 ( 제공 )
+		
+			// owner 는 pb_resouce.owner = pb 
+			var data = 'section='+section+'&'+'owner='+params;
+
+			$.ajax({
+				type:"post"		// 포스트방식
+				,url:url		// url 주소
+				,data:params	//  요청에 전달되는 프로퍼티를 가진 객체
+				,dataType:"json"
+				,success:function(args){	//응답이 성공 상태 코드를 반환하면 호출되는 함수
+					_handler(args);
+				}
+				,error:function(e) {	// 이곳의 ajax에서 에러가 나면 얼럿창으로 에러 메시지 출력
+					alert(e.responseText);
+				}
+			});
+		}
+	}); // $(function()
 }); // require
--- js/pb/ui/dlg_add_image.js
+++ js/pb/ui/dlg_add_image.js
@@ -1,14 +1,15 @@
 require(['jquery', 'underscore', 'jquery_ui', 'jquery_ui_custom'],
 	function ($, _, jquery_ui, jquery_ui_custom) {
 		//console.log("dlg_add_image");
-
 		var dlg_addImage = pb.ui.dialog('dlg_add_image', { isCenter : false });
 		var $dlg_addImage = dlg_addImage.jquerySelector;
-		console.log($dlg_addImage);
 
 		$("img", $dlg_addImage).draggable({
 			containment: "document",    // 드래그로 움직임이 가능함 기준점을 설정함
 			helper: "clone",   // 드래깅할때 디스플레이되 관련 설정
 			appendTo: $(".scene")   // 드래그한걸 어디에 붙일 것인가
 		}); // draggable()
+
+		//console.log('resource : '+'pb');
+		//pb.current.resource = pb.io.ajax.db('resource','pb');
 	}); // function()
