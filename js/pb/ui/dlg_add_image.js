4bd7c98a5651eb6fa8362114bb377658afd080d3
hong <hong@dev.hitit.kr>
2014-10-23 13:49:50 +0900

    db데이터로부터 이미지 파일 불러오기 & 드래그 잘됨+하단 타임라인수정

--- js/pb/ui/dlg_add_image.js
+++ js/pb/ui/dlg_add_image.js
@@ -4,12 +4,20 @@ require(['jquery', 'underscore', 'jquery_ui', 'jquery_ui_custom'],
 		var dlg_addImage = pb.ui.dialog('dlg_add_image', { isCenter : false });
 		var $dlg_addImage = dlg_addImage.jquerySelector;
 
-		$("img", $dlg_addImage).draggable({
-			containment: "document",    // 드래그로 움직임이 가능함 기준점을 설정함
-			helper: "clone",   // 드래깅할때 디스플레이되 관련 설정
-			appendTo: $(".scene")   // 드래그한걸 어디에 붙일 것인가
-		}); // draggable()
+		pb.io.ajax.db('resource','pb', function(result){
+			$('ul#sortable').html('');
+			for (var i in result){
+				console.log(result[i]);
+				$('ul#sortable').append('<li class="ui-state-default">$1<img src="$2" width="100" height="100" class="ui-draggable ui-draggable-handle"></li>'.replace('$1',result[i].showName).replace('$2',result[i].imgSrc));
+			}
 
-		//console.log('resource : '+'pb');
-		//pb.current.resource = pb.io.ajax.db('resource','pb');
+			// 드래그 할 수 있게
+			$("img", $dlg_addImage).draggable({
+				containment: "document",    // 드래그로 움직임이 가능함 기준점을 설정함
+				helper: "clone",   // 드래깅할때 디스플레이되 관련 설정
+				appendTo: $(".scene")   // 드래그한걸 어디에 붙일 것인가
+			}); // draggable()
+		});
+
+
 	}); // function()
